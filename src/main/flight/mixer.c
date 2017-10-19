9349c28454d55b34613e35053d2c7bdc538b5469
Michel Pastor <shellixyz@gmail.com>
2017-10-20 01:53:44 +0200

    flaperon_pitch_comp

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -74,7 +74,8 @@ PG_REGISTER_WITH_RESET_TEMPLATE(mixerConfig_t, mixerConfig, PG_MIXER_CONFIG, 0);
 PG_RESET_TEMPLATE(mixerConfig_t, mixerConfig,
     .mixerMode = MIXER_QUADX,
     .yaw_motor_direction = 1,
-    .yaw_jump_prevention_limit = 200
+    .yaw_jump_prevention_limit = 200,
+    .flaperon_pitch_comp_angle = 0
 );
 
 #ifdef BRUSHED_MOTORS
