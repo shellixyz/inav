3b7baaafe8f3c6db626f531981f653735722ef0d
giacomo892 <giacomo892@users.noreply.github.com>
2018-05-23 12:18:40 +0200

    hacks to have control of roll and thr in CRUISE

--- src/main/navigation/navigation_fixedwing.c
+++ src/main/navigation/navigation_fixedwing.c
@@ -577,7 +577,7 @@ void applyFixedWingNavigationController(navigationFSMStateFlags_t navStateFlags,
 #endif
 
         //if (navStateFlags & NAV_CTL_YAW)
-        if ((navStateFlags & NAV_CTL_ALT) || (navStateFlags & NAV_CTL_POS))
+        if ((navStateFlags & NAV_CTL_ALT) || (navStateFlags & NAV_CTL_POS & !FLIGHT_MODE(NAV_CRUISE_MODE)))
             applyFixedWingPitchRollThrottleController(navStateFlags, currentTimeUs);
     }
 }
