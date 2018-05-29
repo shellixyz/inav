f5022ba25c048c5f519acb81354c59f3970c8cba
giacomo892 <giacomo892@users.noreply.github.com>
2018-05-29 22:36:29 +0200

    fix!

--- src/main/navigation/navigation.c
+++ src/main/navigation/navigation.c
@@ -952,18 +952,20 @@ static navigationFSMEvent_t navOnEnteringState_NAV_STATE_CRUISE_2D_IN_PROGRESS(n
     DEBUG_SET(DEBUG_CRUISE, 1, 0); //no ajusting
     
     if((previousState == NAV_STATE_CRUISE_2D_ADJUSTING) || posControl.flags.isAdjustingHeading){
-      resetPositionController();  //user is rolling bypass the controller and go the ANGLE.
-      calculateFarAwayTarget(&posControl.cruise.cruiseTargetPos, posControl.cruise.cruiseYaw, 50000); //calculate a 500m far away target when user changed direction
+       calculateFarAwayTarget(&posControl.cruise.cruiseTargetPos, posControl.cruise.cruiseYaw, 50000); //calculate a 500m far away target when user changed direction
        DEBUG_SET(DEBUG_CRUISE, 1, 1); //adj
     }
 
     else if(calculateDistanceToDestination(&posControl.cruise.cruiseTargetPos) < 10000) //100m
     {   
         calculateNewCruiseTarget(&posControl.cruise.cruiseTargetPos, posControl.cruise.cruiseYaw, 50000); //500m apart
-        setDesiredPosition(&posControl.cruise.cruiseTargetPos, posControl.cruise.cruiseYaw, NAV_POS_UPDATE_XY | NAV_POS_UPDATE_HEADING);
         DEBUG_SET(DEBUG_CRUISE, 1, 2); //renew
     }
-    DEBUG_SET(DEBUG_CRUISE, 3, posControl.actualState.yaw/100); //log yaw    
+
+    setDesiredPosition(&posControl.cruise.cruiseTargetPos, posControl.cruise.cruiseYaw, NAV_POS_UPDATE_XY | NAV_POS_UPDATE_HEADING);
+
+    DEBUG_SET(DEBUG_CRUISE, 3, posControl.cruise.cruiseYaw/100); //log yaw 
+
     return NAV_FSM_EVENT_NONE;  
 }
 
