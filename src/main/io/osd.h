5d168f86545471fbf72c153edce52264adbc3189
Michel Pastor <shellixyz@gmail.com>
2018-05-29 17:04:24 +0200

    Fix osd.h

--- src/main/io/osd.h
+++ src/main/io/osd.h
@@ -93,8 +93,6 @@ typedef enum {
     OSD_WIND_SPEED_HORIZONTAL,
     OSD_WIND_SPEED_VERTICAL,
     OSD_DEBUG, // Number 46. Intentionally absent from configurator and CMS. Set it from CLI.
-    OSD_WIND_SPEED_HORIZONTAL,
-    OSD_WIND_SPEED_VERTICAL,
     OSD_ITEM_COUNT // MUST BE LAST
 } osd_items_e;
 
